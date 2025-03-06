3778f725ef3499b96019772fc4dc6b5ba0c4d790
Mohikshit Ghorai <gmohikshit@gmail.com>
2025-03-06 11:32:47 +0530

    Stack Screens in _layout.tsx(root)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,46 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
-// Import your global CSS file
-import "../global.css";
+// Prevents hiding the Splash Screen when app loads
+SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  return <Stack />;
+export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+
+  // To render the application once all the fonts are loaded
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(home)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tools)" options={{ headerShown: false }} />
+        <Stack.Screen name="(transcript)" options={{ headerShown: false }} />
+        <Stack.Screen name="(fsessions)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
 }
