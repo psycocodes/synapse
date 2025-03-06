046e0b66e0b1d66f38a411bee10a6062655c08eb
Suparno Saha <99113360+letsbecool9792@users.noreply.github.com>
2025-03-06 21:02:43 +0530

    added assets and constants

---
+++ constants/images.js
@@ -0,0 +1,14 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+import logoSmall from "../assets/images/logo-small.png";
+import empty from "../assets/images/empty.png";
+import notebookCreate from "../assets/images/notebook-create.png";
+import groupCreate from "../assets/images/group-create.png";
+import noFiles from "../assets/images/nofiles.png";
+import bg from "../assets/images/bg.png";
+import studentAnimated from "../assets/images/student.gif";
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, notebookCreate, groupCreate, noFiles, bg, studentAnimated};
