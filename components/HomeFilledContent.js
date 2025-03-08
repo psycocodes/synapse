acb0595448407884d7b721e4e20c579e1526cdb7
Suparno Saha <99113360+letsbecool9792@users.noreply.github.com>
2025-03-08 14:32:05 +0530

    removed play button

--- components/HomeFilledContent.js
+++ components/HomeFilledContent.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { View, Text, TouchableOpacity, FlatList, StyleSheet, Animated, Pressable } from 'react-native';
 import { FAB, IconButton, useTheme } from 'react-native-paper';
@@ -6,27 +5,22 @@ import ItemType from "../constants/ItemType";
 import LinearGradient from 'react-native-linear-gradient';
 import tinycolor from 'tinycolor2';
 
+const COLOR_3_btnGradB = 'rgb(28, 4, 50)'; // 0.4*Color2 | prev: 'rgb(27, 0, 43)';
 
-const COLOR_3_btnGradB   = 'rgb(28, 4, 50)';    // 0.4*Color2              | prev: 'rgb(27, 0, 43)';
-
-const ShiningListItem = ({children, iconComponent, style, innerStyle, onPress }) => {
+const ShiningListItem = ({ children, style, innerStyle, onPress }) => {
     const shimmerAnim = React.useRef(new Animated.Value(0)).current;
 
     const handlePress = () => {
-        // Reset animation value and animate the overlay from left to right.
         shimmerAnim.setValue(0);
         Animated.timing(shimmerAnim, {
             toValue: 1,
             duration: 300,
             useNativeDriver: true,
         }).start(() => {
-            // After animation completes, invoke the onPress callback.
             if (onPress) onPress();
         });
     };
 
-    // Interpolate the animation value to calculate a translateX value.
-    // Adjust the outputRange as needed based on your item’s width.
     const shimmerTranslateX = shimmerAnim.interpolate({
         inputRange: [0, 1],
         outputRange: [-200, 300],
@@ -41,21 +35,17 @@ const ShiningListItem = ({children, iconComponent, style, innerStyle, onPress })
     });
 
     return (
-        /* Wrapping view with overflow hidden to clip the animated overlay */
-        <Animated.View style={[style, { overflow: 'hidden', opacity: mainOpacity}]}>
-
+        <Animated.View style={[style, { overflow: 'hidden', opacity: mainOpacity }]}>
             <Pressable onPressIn={handlePress} style={innerStyle} unstable_pressDelay={100}>
                 {children}
             </Pressable>
-
-            {iconComponent} 
 
             <Animated.View
                 style={{
                     position: 'absolute',
                     top: 0,
                     bottom: 0,
-                    width: 200, // width of the shimmering overlay; adjust as needed
+                    width: 200,
                     transform: [{ translateX: shimmerTranslateX }],
                     opacity: shimmerOpacity,
                 }}
@@ -80,8 +70,7 @@ export default function ({ items, onItemPress, openDialog }) {
             style={styles.itemContainer}
             onPress={() => onItemPress(item)}
             innerStyle={styles.touchableContainer}
-            iconComponent={<IconButton icon="play" iconColor={theme.colors.primary} size={30}/>}>
-
+        >
             <IconButton
                 icon={item.type === ItemType.NOTEBOOK ? 'note-text' : 'folder-open'}
                 iconColor={
@@ -102,7 +91,6 @@ export default function ({ items, onItemPress, openDialog }) {
             <FlatList
                 data={items}
                 renderItem={renderItem}
-                // keyExtractor={(item) => item.name} // assuming names are unique
                 keyExtractor={(item, index) => index.toString()}
             />
             <FAB
@@ -112,7 +100,7 @@ export default function ({ items, onItemPress, openDialog }) {
             />
         </View>
     );
-};
+}
 
 export const createStyles = (theme) => StyleSheet.create({
     container: {
@@ -131,7 +119,7 @@ export const createStyles = (theme) => StyleSheet.create({
         backgroundColor: tinycolor(theme.colors.onPrimary).setAlpha(0.3).toString()
     },
     touchableContainer: {
-        flex:1,
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         padding: 8,
